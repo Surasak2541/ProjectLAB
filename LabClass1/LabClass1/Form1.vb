f15b0d74f02911f073f01d911d998879c03110ef
Fam <surasuk145632@gmail.com>
2018-12-06 13:30:23 +0700

    projectFRM1-2

---
+++ LabClass1/LabClass1/Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class Form1
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim salary, salaryperyear, tex As Double
+        salary = txtSalary.Text
+        salary = Val(salary * 12)
+
+
+        salaryperyear = Val(salary * 5) / 100
+
+
+        tex = Val(salary) - Val(salaryperyear)
+
+        lblSalaryperyear.Text = salary
+        lblTex.Text = salaryperyear
+        lblNetSalary.Text = tex
+
+
+
+
+
+
+
+
+
+
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+End Class
---
+++ LabClass1/LabClass1/Form2.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form2
+    Private Sub Label13_Click(sender As Object, e As EventArgs) Handles lbltotalcommission.Click, lbltotalVistraANDComputer.Click, lblcomcomputer.Click
+
+    End Sub
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim vistra, computer, totalvisandcom, totalcommission, commisvis, commiscom As Double
+        vistra = txtvistra.Text
+        computer = txtcomputer.Text
+        totalvisandcom = vistra + computer
+        lbltotalVistraANDComputer.Text = totalvisandcom
+        commisvis = vistra * 0.05
+        lblcomvistra.Text = commisvis
+        commiscom = computer * 0.1
+        lblcomcomputer.Text = commiscom
+        totalcommission = commisvis + commiscom
+        lbltotalcommission.Text = totalcommission
+
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.Close()
+    End Sub
+End Class
